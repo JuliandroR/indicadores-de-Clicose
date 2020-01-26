58db2d8f11643ad26015270df1dc0e3f6c6abfa2
JuliandroR <juliandro.rocha@gmail.com>
2020-01-26 19:10:21 -0400

    Implementa o Armazenamento dos dados medidos

--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,42 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-    onDeviceReady: function() {
-        // this.receivedEvent('deviceready');
-        document.getElementById("nova_medicao").addEventListener("click", () => {
-            window.location.assign("pages/nova_medicao.html");
-        })
-    },
+let adapter = new LocalStorage("db");
+let db = low(adapter);
 
-    // receivedEvent: function(id) {
-    // }
+if (localStorage.createDatabase != "true") {
+  db.defaults({
+    medicoes: []
+  }).write();
+  localStorage.createDatabase = "true";
+}
+
+document.getElementById("nova_medicao").addEventListener("click", () => {
+  window.location.assign("pages/nova_medicao.html");
+});
+
+const listarMedicoes = () => {
+  let data = db.get("medicoes").value();
+
+  data.forEach(item => {
+    document
+      .getElementById("medicoes_anteriores")
+      .appendChild(criaElemento(item.dataMedicao, item.valorMedido));
+  });
 };
 
-app.initialize();
+const criaElemento = (dataMedicao, valorMedido) => {
+  let div = document.createElement("div");
+  div.setAttribute("class", "medicao");
+
+  let h3 = document.createElement("h3");
+  h3.setAttribute("class", "data");
+  h3.innerHTML = dataMedicao;
+  div.appendChild(h3);
+
+  let h2 = document.createElement("h2");
+  h2.setAttribute("class", "valor");
+  h2.innerHTML = valorMedido;
+  div.appendChild(h2);
+
+  return div;
+};
+
+listarMedicoes();
